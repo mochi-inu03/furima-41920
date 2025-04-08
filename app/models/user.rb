098df17a6d5d0ba7524f035c257ceba6f0d167b8
mochi-inu03 <nissyuika@gmail.com>
2025-04-08 13:59:56 +0900

    バリデーション&トップページ

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :read_first, presence: true
+  validates :read_last, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :birth_day, presence: true
 end
