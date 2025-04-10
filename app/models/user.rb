30aa85c0ee1869280cdf0a39154dfc6b78f98d6b
mochi-inu03 <nissyuika@gmail.com>
2025-04-10 09:54:06 +0900

    新規登録

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :read_first, presence: true
-  validates :read_last, presence: true
-  validates :email, presence: true, uniqueness: true
-  validates :birth_day, presence: true
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  with_options presence: true do
+    validates :nickname
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' }
+    validates :read_first, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :read_last, format: { with: /\A[ァ-ヶー]+\z/, message: '全角カタカナを使用してください' }
+    validates :email, uniqueness: true
+    validates :birth_day
+  end
 end
