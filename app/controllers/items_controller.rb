7be5e2d7a6b11f0efe0ca5ba7c8c598a8b960a05
mochi-inu03 <nissyuika@gmail.com>
2025-04-28 16:43:24 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,7 @@
 class ItemsController < ApplicationController
-
- before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update]
   before_action :move_to_root_path, only: [:edit, :update]
- 
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -23,7 +21,6 @@ class ItemsController < ApplicationController
   end
 
   def show
-
   end
 
   def edit
@@ -35,7 +32,13 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
-    
+  end
+
+  def destroy
+    item = Item.find(params[:id])
+    item.destroy
+    redirect_to root_path
+  end
 
   private
 
@@ -50,6 +53,7 @@ class ItemsController < ApplicationController
 
   def move_to_root_path
     return unless current_user.id != @item.user_id
+
     redirect_to root_path
   end
 end
